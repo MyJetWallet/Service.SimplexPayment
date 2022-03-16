b28a77ec78ee287e7b671b76ab80286d21c09720
Pavlo Pylypenko <pavel.p@smplt.net>
2022-03-16 14:03:23 +0200

    added indecies

---
+++ src/Service.SimplexPayment.Postgres/Migrations/20220316120240_Indecies.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Service.SimplexPayment.Postgres.Migrations
+{
+    public partial class Indecies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_intentions_ClientIdHash",
+                schema: "simplex",
+                table: "intentions",
+                column: "ClientIdHash");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_intentions_PaymentId",
+                schema: "simplex",
+                table: "intentions",
+                column: "PaymentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_intentions_ClientIdHash",
+                schema: "simplex",
+                table: "intentions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_intentions_PaymentId",
+                schema: "simplex",
+                table: "intentions");
+        }
+    }
+}
